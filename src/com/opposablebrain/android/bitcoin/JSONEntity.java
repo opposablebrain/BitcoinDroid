24029a6c7e2f27ee5bf7c39455ca16d53ee82f9b
Rodin <rodin@zee.aero>
2010-07-21 23:53:05 -0700

    First working demo.

---
+++ src/com/opposablebrain/android/bitcoin/JSONEntity.java
@@ -0,0 +1,26 @@
+package org.alexd.jsonrpc;
+
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.Header;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+
+/**
+ * Provides a HttpEntity for json content
+ */
+class JSONEntity extends StringEntity 
+{
+	public JSONEntity(JSONObject jsonObject) throws UnsupportedEncodingException 
+	{
+		super(jsonObject.toString());
+	}
+
+	@Override
+	public Header getContentType() 
+	{
+		return new BasicHeader(HTTP.CONTENT_TYPE, "application/json");
+	}	
+}
