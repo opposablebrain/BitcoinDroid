6361ffac0bc3ce87a2fd54c6e8e7d856e82b2a4a
Rodin <rodin@zee.aero>
2010-07-23 07:57:45 -0700

    Updates.

--- gen/com/opposablebrain/android/bitcoin/R.java
+++ gen/com/opposablebrain/android/bitcoin/R.java
@@ -13,6 +13,13 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int login_button=0x7f050003;
+        public static final int password=0x7f050002;
+        public static final int port=0x7f050001;
+        public static final int result=0x7f050004;
+        public static final int server=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
